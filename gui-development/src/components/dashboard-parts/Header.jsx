200a6f561bfcf658aaa2136a01cf0ea7d0d71413
karim <akskarim17@gmail.com>
2023-01-14 00:20:17 +0600

    renamed to header from topbar

---
+++ gui-development/src/components/dashboard-parts/Header.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import url from "../../util/URL";
+import { Link } from "react-router-dom";
+
+function MenuLink({ to, children, ...props }) {
+  return (
+    <Link
+      className="inline-block w-full px-2 cursor-pointer hover:bg-gray-100 font-sans"
+      {...props}
+      to={to}>
+      {children}
+    </Link>
+  );
+}
+
+function Menu() {
+  return (
+    <div className="absolute right-4 shadow-2xl rounded-sm bg-white z-20">
+      <MenuLink to="/profile">Profile</MenuLink>
+      <MenuLink to="/settings">Settings</MenuLink>
+      <hr />
+      <MenuLink to="/logout">Logout</MenuLink>
+    </div>
+  );
+}
+
+export default function Header({ ...props }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <header className="relative" {...props}>
+      <div className="h-full bg-white shadow-xl p-2 flex justify-between items-center">
+        <h4 className="font-lato text-lg font-semibold">Title here</h4>
+        <div>
+          <span
+            className="inline-block h-10 bg-white-500 border-2 border-emerald-300 rounded-full p-1 cursor-pointer"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <img
+              src={`${url.image}/user.png`}
+              alt="user"
+              className="max-h-full"
+            />
+          </span>
+        </div>
+      </div>
+      {isMenuOpen && <Menu />}
+    </header>
+  );
+}
