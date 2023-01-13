eb80a3c85ca972e70011c2a5392c505bb4ecebd5
karim <akskarim17@gmail.com>
2023-01-14 00:18:29 +0600

    padding added

--- gui-development/src/pages/Dashboard.jsx
+++ gui-development/src/pages/Dashboard.jsx
@@ -1,38 +1,42 @@
 import { useEffect } from "react";
-import { useState } from "react"
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/dashboard-parts/Sidebar";
-import Topbar from "../components/dashboard-parts/Topbar";
+import Header from "../components/dashboard-parts/Header";
 import useLoading from "../hooks/useLoading";
 import http from "../util/http";
 
 export default function Dashboard({ children }) {
-    const [userData, setUserData] = useState({});
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
-    useLoading(loading);
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  useLoading(loading);
 
-    useEffect(() => {
-        http
-            .get('/user/me')
-            .then(res => {
-                setUserData(res.data.data)
-            })
-            .catch(() => {
-                navigate('/login')
-            })
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [navigate])
+  useEffect(() => {
+    http
+      .get("/user/me")
+      .then((res) => {
+        setUserData(res.data.data);
+      })
+      .catch(() => {
+        navigate("/login");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [navigate]);
 
-    return userData !== {} &&
-        <div>
-            <Topbar />
-            <Sidebar />
-            <main>
-                {children}
-            </main>
+  return (
+    userData !== {} && (
+      <div>
+        <Header />
+        <div className="flex">
+          <Sidebar />
+          <main className="bg-white w-full m-2 mr-0 shadow-lg relative z-10 rounded-sm p-2">
+            {children}
+          </main>
         </div>
-
+      </div>
+    )
+  );
 }
