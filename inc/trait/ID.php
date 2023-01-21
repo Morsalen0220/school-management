8b2f10d275878df074fe4af636fce5696c812cfa
unknown <akskarim17@gmail.com>
2023-01-21 23:10:23 +0600

    formatted

--- inc/trait/ID.php
+++ inc/trait/ID.php
@@ -3,53 +3,53 @@
  * Handle user id
  */
 
-trait ID {
+trait ID
+{
+  /**
+   * Generate user id
+   */
+  private static function generate_id(): int
+  {
+    # Get last user id and increment one
+    $user = Query::get_specific(
+      self::$_table_name,
+      ["user_id"],
+      [
+        "1" => "1",
+      ],
+      false,
+      1
+    );
 
-    /**
-     * Generate user id
-     */
-    private static function generate_id(): int {
-        # Get last user id and increment one
-        $user = Query::get_specific(
-            self::$_table_name,
-            [
-                'user_id',
-            ],
-            [
-                '1' => '1',
-            ],
-            false,
-            1
-        );
-
-        # if id not found create an id
-        if (count($user) === 0) {
-            return self::$_initial_user_id;
-        }
-
-        return $user->user_id + 1;
+    # if id not found create an id
+    if (count($user) === 0) {
+      return self::$_initial_user_id;
     }
 
-    /**
-     * Generate random unique id
-     * @return int return id
-     */
-    private static function generate_random_id(): int {
-        $numbers = range(1, 13);
-        shuffle($numbers);
-        return (int) join('', $numbers);
-    }
+    return $user->user_id + 1;
+  }
 
-    /**
-     * Handle invalid
-     * return error response on invalid id
-     *
-     * @param string id
-     */
-    private static function handle_invalid_id(string $id): void {
-        if (0 === preg_match('/^[0-9]+$/', $id)) {
-            send_response(false, 400, ['invalid id']);
-        }
-    }
+  /**
+   * Generate random unique id
+   * @return int return id
+   */
+  private static function generate_random_id(): int
+  {
+    $numbers = range(1, 13);
+    shuffle($numbers);
+    return (int) join("", $numbers);
+  }
 
+  /**
+   * Handle invalid
+   * return error response on invalid id
+   *
+   * @param string id
+   */
+  private static function handle_invalid_id(string $id): void
+  {
+    if (0 === preg_match('/^[0-9]+$/', $id)) {
+      send_response(false, 400, ["invalid id, only numbers are allowed"]);
+    }
+  }
 }
