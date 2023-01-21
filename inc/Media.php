1b0a1ef575f572330e0f7cf3d4423aace5d0784a
unknown <akskarim17@gmail.com>
2023-01-21 23:13:23 +0600

    user and school id initiated

--- inc/Media.php
+++ inc/Media.php
@@ -7,60 +7,73 @@
  * Include config file
  * config file contains max image upload size
  */
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/CustomException.php';
+require_once __DIR__ . "/../config.php";
+require_once __DIR__ . "/CustomException.php";
 
-class Media extends CustomException {
-    private $_image_folder   = __DIR__ . '/../public/uploads/image/';
-    private $_valid_image    = ['jpeg', 'gif', 'png'];
-    private $_maxImageUpload = (1048576 * MAX_IMAGE_UPLOAD_SIZE); # Mb to bytes
+class Media extends CustomException
+{
+  private $_image_folder = __DIR__ . "/../public/uploads/image/";
+  private $_valid_image = ["jpeg", "gif", "png"];
+  private $_maxImageUpload = 1048576 * MAX_IMAGE_UPLOAD_SIZE; # Mb to bytes
+  /**
+   * Initiate file
+   * @param file
+   */
+  public function __construct($file)
+  {
+    $this->_file = $file;
+  }
 
-    /**
-     * Initiate file
-     * @param file
-     */
-    public function __construct($file) {
-        $this->_file = $file;
+  /**
+   * Handle image
+   * validate and move to location
+   * @param string prefix
+   * @return array image info
+   */
+  public function handle_image(string $prefix = ""): string
+  {
+    # Get file info
+    $info = getimagesize($this->_file["tmp_name"]);
+
+    # is valid image
+    if (
+      !is_array($info) ||
+      !in_array(str_replace("image/", "", $info["mime"]), $this->_valid_image)
+    ) {
+      throw new CustomException(
+        "invalid image! accepted format are " . join(", ", $this->_valid_image)
+      );
     }
 
-    /**
-     * Handle image
-     * validate and move to location
-     * @param string prefix
-     * @return array image info
-     */
-    public function handle_image(string $prefix = ''): string {
-        # Get file info
-        $info = getimagesize($this->_file['tmp_name']);
-
-        # is valid image
-        if (!is_array($info) || !in_array(str_replace('image/', '', $info['mime']), $this->_valid_image)) {
-            throw new CustomException("invalid image! accepted format are " . join(', ', $this->_valid_image));
-        }
-
-        if ($this->_file['size'] > $this->_maxImageUpload) {
-            throw new CustomException("file size is too large! maximum image upload size is " . MAX_IMAGE_UPLOAD_SIZE . "MB");
-        }
-
-        $name = $prefix . md5(time() + rand()) . '.jpg';
-
-        # Move image
-        $imageFN = "imagecreatefrom" . str_replace('image/', '', $info['mime']);
-        $image   = call_user_func($imageFN, $this->_file['tmp_name']);
-
-        if (!imagejpeg($image, $this->_image_folder . $name, 75)) {
-            throw new CustomException('"image uploading failed", please try again later!');
-        }
-
-        return $name;
+    if ($this->_file["size"] > $this->_maxImageUpload) {
+      throw new CustomException(
+        "file size is too large! maximum image upload size is " .
+          MAX_IMAGE_UPLOAD_SIZE .
+          "MB"
+      );
     }
 
-    /**
-     * Delete image
-     * @return bool is deleted
-     */
-    public function delete_image(): bool {
-        return unlink($this->_image_folder . $this->_file);
+    $name = $prefix . md5(time() + rand()) . ".jpg";
+
+    # Move image
+    $imageFN = "imagecreatefrom" . str_replace("image/", "", $info["mime"]);
+    $image = call_user_func($imageFN, $this->_file["tmp_name"]);
+
+    if (!imagejpeg($image, $this->_image_folder . $name, 75)) {
+      throw new CustomException(
+        '"image uploading failed", please try again later!'
+      );
     }
 
+    return $name;
+  }
+
+  /**
+   * Delete image
+   * @return bool is deleted
+   */
+  public function delete_image(): bool
+  {
+    return unlink($this->_image_folder . $this->_file);
+  }
 }
--- inc/Request.php
+++ inc/Request.php
@@ -7,112 +7,123 @@
  * Implement autoload for controller classes
  */
 spl_autoload_register(function ($className) {
-    $file = __DIR__ . '/../api/controller/' . $className . '.php';
-    if (file_exists($file)) {
-        require_once $file;
-    }
+  $file = __DIR__ . "/../api/controller/" . $className . ".php";
+  if (file_exists($file)) {
+    require_once $file;
+  }
 });
 
-class Request {
-    # Targeted request
-    private $_request = null;
+class Request
+{
+  # Targeted request
+  private $_request = null;
 
-    /**
-     * Initiate request
-     * @param RequestObject incoming request
-     */
-    public function __construct($request) {
-        $this->_request = $request;
+  /**
+   * Initiate request
+   * @param RequestObject incoming request
+   */
+  public function __construct($request)
+  {
+    $this->_request = $request;
+  }
+
+  /**
+   * Handle get request
+   * @param string class name
+   * @param string method name
+   * @param string parameter
+   * @return Request
+   */
+  public function get(
+    string $class,
+    string $method,
+    string $parameter = ""
+  ): Request {
+    if ($this->_request["REQUEST_METHOD"] === "GET") {
+      call_user_func([$class, $method], $parameter);
+    }
+    return $this;
+  }
+
+  /**
+   * Handle post request
+   * @param string class name
+   * @param string method name
+   * @param string parameter
+   * @return Request
+   */
+  public function post(string $class, string $method, $parameter = ""): Request
+  {
+    if ($this->_request["REQUEST_METHOD"] === "POST") {
+      call_user_func([$class, $method], $parameter);
+    }
+    return $this;
+  }
+
+  /**
+   * Handle patch request
+   * @param string class name
+   * @param string method name
+   * @param string parameter
+   * @return Request
+   */
+  public function patch(string $class, string $method, $parameter = ""): Request
+  {
+    if ($this->_request["REQUEST_METHOD"] === "PATCH") {
+      call_user_func([$class, $method], $parameter);
+    }
+    return $this;
+  }
+
+  /**
+   * Handle delete request
+   * @param string class name
+   * @param string method name
+   * @param string parameter
+   * @return Request
+   */
+  public function delete(
+    string $class,
+    string $method,
+    $parameter = ""
+  ): Request {
+    if ($this->_request["REQUEST_METHOD"] === "DELETE") {
+      call_user_func([$class, $method], $parameter);
+    }
+    return $this;
+  }
+
+  /**
+   * Handle options request
+   * @return Request
+   */
+  public function options(): Request
+  {
+    if ($this->_request["REQUEST_METHOD"] === "OPTIONS") {
+      http_response_code(204);
+      exit();
+    }
+    return $this;
+  }
+
+  /**
+   * Handle authentication
+   * @param string user role
+   */
+  public function auth(string $role): Request
+  {
+    $data = User::verify_session();
+
+    if ($data["user_role"] !== $role && $role !== "any") {
+      send_response(false, 403, [
+        'you don\'t have access to complete this request',
+      ]);
     }
 
-    /**
-     * Handle get request
-     * @param string class name
-     * @param string method name
-     * @param string parameter
-     * @return Request
-     */
-    public function get(string $class, string $method, string $parameter = ''): Request {
-        if ($this->_request['REQUEST_METHOD'] === 'GET') {
-            call_user_func(array($class, $method), $parameter);
-        }
-        return $this;
-    }
+    define("LOGGEDIN_IN_USER_ROLE", $data["user_role"]);
+    define("LOGGEDIN_IN_USER_ID", $data["user_id"]);
+    define("LOGGEDIN_IN_USER_SCHOOL_ID", ""); // TODO
 
-    /**
-     * Handle post request
-     * @param string class name
-     * @param string method name
-     * @param string parameter
-     * @return Request
-     */
-    public function post(string $class, string $method, $parameter = ''): Request {
-        if ($this->_request['REQUEST_METHOD'] === 'POST') {
-            call_user_func(array($class, $method), $parameter);
-        }
-        return $this;
-    }
-
-    /**
-     * Handle patch request
-     * @param string class name
-     * @param string method name
-     * @param string parameter
-     * @return Request
-     */
-    public function patch(string $class, string $method, $parameter = ''): Request {
-        if ($this->_request['REQUEST_METHOD'] === 'PATCH') {
-            call_user_func(array($class, $method), $parameter);
-        }
-        return $this;
-    }
-
-    /**
-     * Handle delete request
-     * @param string class name
-     * @param string method name
-     * @param string parameter
-     * @return Request
-     */
-    public function delete(string $class, string $method, $parameter = ''): Request {
-        if ($this->_request['REQUEST_METHOD'] === 'DELETE') {
-            call_user_func(array($class, $method), $parameter);
-        }
-        return $this;
-    }
-
-    /**
-     * Handle options request
-     * @return Request
-     */
-    public function options(): Request {
-        if ($this->_request['REQUEST_METHOD'] === 'OPTIONS') {
-            http_response_code(204);
-            exit;
-        }
-        return $this;
-    }
-
-    /**
-     * Handle authentication
-     * @param string user role
-     */
-    public function auth(string $role): Request {
-        $data = User::verify_session();
-
-        /**
-         * Match logged in user role with provided role
-         */
-        if ($data['user_role'] !== $role) {
-            send_response(false, 403, ['you don\'t have access to complete this request']);
-        }
-
-        /**
-         * Define user role and user id to access later in code
-         */
-        define('LOGGEDIN_IN_USER_ROLE', $data['user_role']);
-        define('LOGGEDIN_IN_USER_ID', $data['user_id']);
-
-        return $this;
-    }
+    return $this;
+  }
 }
