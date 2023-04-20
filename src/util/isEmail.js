721d4c81a2d3ff3fb3c6ec7aadd3cec19d532a56
unknown <akskarim17@gmail.com>
2023-04-20 20:10:15 +0600

    fixed regex - Resolved #8

--- src/util/isEmail.js
+++ src/util/isEmail.js
@@ -5,35 +5,17 @@
  */
 export default function isEmail(email) {
   /**
-   * Regex
+   **Explanation:
    *
-   ** username group
-   * start with char_u
-   *
-   * OPTIONAL group
-   * - start with (.)
-   * - char_u group
-   *
-   ** @
-   *
-   ** domain group
-   * start with char_d
-   * (.)
-   * char_d
-   *
-   * OPTIONAL group
-   * -  char_d
-   * - (.)
-   * - char_d
+   *^ matches the beginning of the string
+   *[\w.%+-]+ matches one or more word characters, or %, ., + or -
+   *@ matches the @ symbol
+   *[A-a0-9.-]+ matches one or more characters that are letters (either lowercase or uppercase), digits, ., or -
+   *\. matches a literal dot (since . is a special character in regex)
+   *[a-zA-Z]{2,} matches two or more letters (either lowercase or uppercase)
+   *$ matches the end of the string
    */
 
-  //   TODO : Solve error
-  //   let match = email
-  //     .trim()
-  //     .match(
-  //       /^(?'username'(?'char_u'[A-z0-9-#`!%$&+*/=?^-_{|}~]+)([.](?&char_u))?)@(?'domain'(?'char_d'[A-z-0-9-_]+)[.](?&char_d)([.](?&char_d))?)$/
-  //     );
-  //   return match !== null;
-
-  return true;
+  let match = email.trim().match(/^[\w.%+-]+@[A-z0-9.-]+\.[a-zA-Z]{2,}$/);
+  return match !== null;
 }
