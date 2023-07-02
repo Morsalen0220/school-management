105ad7b9c971a6e3e069d60381de9ea968798c4a
Karim <akskarim17@gmail.com>
2023-07-02 10:24:07 +0600

    template file added for config.php file

---
+++ template.config.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *! Please Don't edit this file if you don't know what you are doing
+ *
+ ** This file contains all the settings and DataBase configuration
+ *
+ * all configuration will be automatically saved here in installation time
+ *
+ * This file contains the following configuration
+ ** Database credentials
+ ** Database table prefix
+ ** Secret keys
+ ** Core settings
+ */
+
+/**
+ * Database host
+ * require to connect with database
+ */
+define("DATABASE_HOSTNAME", "localhost");
+
+/**
+ * Database name
+ * require to connect with database
+ ** Which database to connect
+ */
+define("DATABASE_NAME", "");
+
+/**
+ * Database user username
+ * require to connect with database
+ ** Username to use in connection
+ */
+define("DATABASE_USERNAME", "");
+
+/**
+ * Database user password
+ * require to connect with database
+ ** Password for the use to use in connection
+ */
+define("DATABASE_PASSWORD", "");
+
+/**
+ * Database charset
+ * require to create table in database
+ */
+define("DATABASE_CHARSET", "");
+
+/**
+ * Database table prefix
+ * Prepend to table name
+ */
+define("TABLE_PREFIX", "");
+
+/**
+ * Default user image name
+ * when user doesn't uploaded an image this image will be in use
+ * Image should be placed in public/uploads/image/ directory with this name
+ */
+define("DEFAULT_USER_IMAGE_NAME", "user.png");
+
+/**
+ * Student id initialization
+ * start student id from
+ */
+define("STUDENT_ID_START_FROM", "000000000000001");
+
+# Maximum image file size in MB
+define("MAX_IMAGE_UPLOAD_SIZE", 10);
+
+# Handle installation
+define("APP_INSTALLED", "0");
+
+/**
+ * Installer IP
+ * If same required to create super admin
+ */
+define("APP_INSTALLER_IP", "");
+
+/**
+ * Environment
+ */
+define("APP_ENVIRONMENT", "production");
+
+/**
+ * Show pdo error mode - bool
+ * turn off in production
+ */
+if (APP_ENVIRONMENT === "development") {
+  define("SHOW_PDO_ERROR", true);
+} else {
+  define("SHOW_PDO_ERROR", false);
+}
