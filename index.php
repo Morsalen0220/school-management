e3fc6ea663e742955df354eea76b4e6c5d89cffb
Morsalen0220 <107208594+Morsalen0220@users.noreply.github.com>
2026-02-17 21:48:09 +0000

    Deploying to bundle from @ Morsalen0220/school-management@142b847207deef2cdc3beb87e27591dce495f796 🚀

--- index.php
+++ index.php
@@ -22,8 +22,73 @@ if (APP_INSTALLED == false) {
   <link
     href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
     rel="stylesheet">
-  <script type="module" crossorigin src="/public/assets/index-1b9cd0c6.js"></script>
-  <link rel="stylesheet" href="/public/assets/index-23be053e.css">
+  <?php
+  $assetDirectory = __DIR__ . '/public/assets';
+
+  $manifestFile = $assetDirectory . '/manifest.json';
+  $cssBundle = '';
+  $jsBundle = '';
+
+  if (file_exists($manifestFile)) {
+    $manifestContent = file_get_contents($manifestFile);
+    $manifest = json_decode($manifestContent ?: '', true);
+
+    if (is_array($manifest) && isset($manifest['index.html'])) {
+      $entry = $manifest['index.html'];
+
+      if (isset($entry['file'])) {
+        $jsBundle = '/public/assets/' . ltrim($entry['file'], '/');
+      }
+
+      if (isset($entry['css'][0])) {
+        $cssBundle = '/public/assets/' . ltrim($entry['css'][0], '/');
+      }
+    }
+  }
+
+  if (empty($jsBundle) || empty($cssBundle)) {
+    if (is_dir($assetDirectory)) {
+      $cssMatches = glob($assetDirectory . '/index-*.css');
+      $jsMatches = glob($assetDirectory . '/index-*.js');
+
+      if (empty($cssBundle) && $cssMatches !== false && !empty($cssMatches)) {
+        usort($cssMatches, static fn($a, $b) => filemtime($b) <=> filemtime($a));
+        $cssBundle = '/public/assets/' . basename($cssMatches[0]);
+      }
+
+      if (empty($jsBundle) && $jsMatches !== false && !empty($jsMatches)) {
+        usort($jsMatches, static fn($a, $b) => filemtime($b) <=> filemtime($a));
+        $jsBundle = '/public/assets/' . basename($jsMatches[0]);
+      }
+
+  $cssBundle = '';
+  $jsBundle = '';
+
+  if (is_dir($assetDirectory)) {
+    $cssMatches = glob($assetDirectory . '/index-*.css');
+    $jsMatches = glob($assetDirectory . '/index-*.js');
+
+    if ($cssMatches !== false && !empty($cssMatches)) {
+      rsort($cssMatches);
+      $cssBundle = '/public/assets/' . basename($cssMatches[0]);
+    }
+
+    if ($jsMatches !== false && !empty($jsMatches)) {
+      rsort($jsMatches);
+      $jsBundle = '/public/assets/' . basename($jsMatches[0]);
+
+    }
+  }
+  ?>
+  <?php if (!empty($cssBundle)) : ?>
+    <link rel="stylesheet" href="<?= $cssBundle ?>">
+  <?php endif; ?>
+
+  <?php if (!empty($jsBundle)) : ?>
+    <script type="module" crossorigin src="<?= $jsBundle ?>"></script>
+  <?php else : ?>
+    <!-- Frontend bundle not found. Run `npm run build` to generate public/assets files. -->
+  <?php endif; ?>
 </head>
 
 <body>
